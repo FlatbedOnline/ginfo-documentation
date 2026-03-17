1b610165c53a206e09be805ac32922b248b49a40
Eduardo Basquiat <flatbed@tutanota.com>
2026-03-17 09:50:03 -0300

    bug fixes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  base: '/ginfo-documentation/',
+  base: '/',
   lang: 'en-US',
 
   title: 'VuePress',
