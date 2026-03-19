60c40e86e27985c2c10b85bb04428c46c190d9d7
Carlos Eduardo <flatbed@tutanota.com>
2026-03-19 09:01:28 -0300

    acréscimo da sessão stress test

--- docs/.vuepress/.temp/pages/faq.html.vue
+++ docs/.vuepress/.temp/pages/faq.html.vue
@@ -1,17 +1,9 @@
-<template><div><h1 id="perguntas-frequentes" tabindex="-1"><a class="header-anchor" href="#perguntas-frequentes"><span>Perguntas frequentes:</span></a></h1>
-<p>O padrão aqui segue:</p>
-<h2 id="p-como-entro-no-sistema" tabindex="-1"><a class="header-anchor" href="#p-como-entro-no-sistema"><span>P: Como entro no sistema?</span></a></h2>
-<p><strong>A: Insira usuário e senha!</strong></p>
-<hr>
-<h1 id="p-pode-retirar-colaboradores-desativados-do-bi" tabindex="-1"><a class="header-anchor" href="#p-pode-retirar-colaboradores-desativados-do-bi"><span>P: Pode retirar colaboradores desativados do BI?</span></a></h1>
+<template><div><h1 id="perguntas-frequentes" tabindex="-1"><a class="header-anchor" href="#perguntas-frequentes"><span>Perguntas frequentes</span></a></h1>
+<h2 id="pode-retirar-colaboradores-desativados-do-bi" tabindex="-1"><a class="header-anchor" href="#pode-retirar-colaboradores-desativados-do-bi"><span>Pode retirar colaboradores desativados do BI?</span></a></h2>
 <p>R: **O BI é apenas um relatório dos dados do Ginfo, então se filtrar pela data em que o colaborador estava ativado, ele ira aparecer nele.</p>
-<h1 id="p-teste-teste-ajustado-4" tabindex="-1"><a class="header-anchor" href="#p-teste-teste-ajustado-4"><span>P: TESTE TESTE AJUSTADO 4</span></a></h1>
-<p>R: TESTES</p>
-<h1 id="checklist" tabindex="-1"><a class="header-anchor" href="#checklist"><span>Checklist</span></a></h1>
-<p>R: **checklist ok **</p>
-<h1 id="p-como-cadastrar-um-colaborador" tabindex="-1"><a class="header-anchor" href="#p-como-cadastrar-um-colaborador"><span>P: Como cadastrar um colaborador?</span></a></h1>
-<p>R: <strong>Iniciar cadastro na tela RH:COLABORADORES&gt;CADASTROS&gt;COLABORADORES(SIMPLES)</strong></p>
-<h1 id="p-como-bater-o-ponto" tabindex="-1"><a class="header-anchor" href="#p-como-bater-o-ponto"><span>P:Como bater o ponto?</span></a></h1>
+<h2 id="como-cadastrar-um-colaborador" tabindex="-1"><a class="header-anchor" href="#como-cadastrar-um-colaborador"><span>Como cadastrar um colaborador?</span></a></h2>
+<p>R: Iniciar cadastro na tela <code v-pre>RH:COLABORADORES&gt;CADASTROS&gt;COLABORADORES(SIMPLES)</code></p>
+<h2 id="como-bater-o-ponto" tabindex="-1"><a class="header-anchor" href="#como-bater-o-ponto"><span>Como bater o ponto?</span></a></h2>
 </div></template>
 
 
---
+++ docs/.vuepress/.temp/pages/stresstest.html.vue
@@ -0,0 +1,4 @@
+<template><div><h1 id="stress-test" tabindex="-1"><a class="header-anchor" href="#stress-test"><span>Stress test</span></a></h1>
+</div></template>
+
+
