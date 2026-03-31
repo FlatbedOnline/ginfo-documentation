841d10d0c8cfbeba4906bc92868b159f58735998
Carlos Eduardo <flatbed@tutanota.com>
2026-03-31 08:58:54 -0300

    att

--- docs/.vuepress/.temp/pages/pneus.html.js
+++ docs/.vuepress/.temp/pages/pneus.html.js
@@ -1,16 +1,3 @@
 import comp from "/home/carlos-eduardo/Documents/ginfo-documentation/docs/.vuepress/.temp/pages/pneus.html.vue"
 const data = JSON.parse("{\"path\":\"/pneus.html\",\"title\":\"Pneus\",\"lang\":\"en-US\",\"frontmatter\":{},\"git\":{\"updatedTime\":1773858923000,\"contributors\":[{\"name\":\"viniciuslomartini-dotcom\",\"username\":\"viniciuslomartini-dotcom\",\"email\":\"viniciuslomartini@gmail.com\",\"commits\":3,\"url\":\"https://github.com/viniciuslomartini-dotcom\"},{\"name\":\"Carlos Eduardo\",\"username\":\"\",\"email\":\"flatbed@tutanota.com\",\"commits\":1}],\"changelog\":[{\"hash\":\"4297eb2152dc722d99afb336f9c51c3b68cb227a\",\"time\":1773858923000,\"email\":\"flatbed@tutanota.com\",\"author\":\"Carlos Eduardo\",\"message\":\"alterações no Pneus\"},{\"hash\":\"8d09a98b6aa14cd0dbc61dc0c5d3786ba8899132\",\"time\":1773854406000,\"email\":\"viniciuslomartini@gmail.com\",\"author\":\"viniciuslomartini-dotcom\",\"message\":\"vault backup: 2026-03-18 14:20:06\"},{\"hash\":\"31520252d601741810d6ce8bce1a9f8a25d4033c\",\"time\":1773854340000,\"email\":\"viniciuslomartini@gmail.com\",\"author\":\"viniciuslomartini-dotcom\",\"message\":\"vault backup: 2026-03-18 14:19:00\"},{\"hash\":\"298835318666f41dbb6c08787db3c8a1c2f79463\",\"time\":1773853328000,\"email\":\"viniciuslomartini@gmail.com\",\"author\":\"viniciuslomartini-dotcom\",\"message\":\"vault backup: 2026-03-18 14:02:07\"}]},\"filePathRelative\":\"pneus.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/stresstest.html.js
+++ docs/.vuepress/.temp/pages/stresstest.html.js
@@ -1,16 +1,3 @@
 import comp from "/home/carlos-eduardo/Documents/ginfo-documentation/docs/.vuepress/.temp/pages/stresstest.html.vue"
 const data = JSON.parse("{\"path\":\"/stresstest.html\",\"title\":\"Stress test\",\"lang\":\"en-US\",\"frontmatter\":{},\"git\":{\"updatedTime\":1773921688000,\"contributors\":[{\"name\":\"Carlos Eduardo\",\"username\":\"\",\"email\":\"flatbed@tutanota.com\",\"commits\":1}],\"changelog\":[{\"hash\":\"c91b5e6d23eee61752198f2c06851b0c7e025bf1\",\"time\":1773921688000,\"email\":\"flatbed@tutanota.com\",\"author\":\"Carlos Eduardo\",\"message\":\"acréscimo da sessão stress test\"}]},\"filePathRelative\":\"stresstest.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
