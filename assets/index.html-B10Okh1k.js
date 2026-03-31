8e44383ca1e5e58ecacc204479e22a5becfa7278
Carlos Eduardo <flatbed@tutanota.com>
2026-03-31 08:35:15 -0300

    Updates

---
+++ assets/index.html-B10Okh1k.js
@@ -0,0 +1,15 @@
+import{_ as s,c as n,a as e,o as i}from"./app-CK1FtWnU.js";const l={};function t(o,a){return i(),n("div",null,[...a[0]||(a[0]=[e(`<h1 id="portal-de-suporte-ginfo" tabindex="-1"><a class="header-anchor" href="#portal-de-suporte-ginfo"><span>Portal de suporte Ginfo</span></a></h1><p>Nessa documentação será abordado tutoriais, FAQs e atualizações frequentes.</p><p>Abaixo um simples tutorial de markdown.</p><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh"><pre><code class="language-bash"><span class="line"><span class="token comment"># Titulo maior</span></span>
+<span class="line"></span>
+<span class="line"><span class="token comment">## Subtitulo</span></span>
+<span class="line"></span>
+<span class="line"><span class="token comment">### sub-sub titulo</span></span>
+<span class="line"></span>
+<span class="line">*itálico*</span>
+<span class="line"></span>
+<span class="line">**bold**</span>
+<span class="line"></span>
+<span class="line">--- ← assim cria separação de linha</span>
+<span class="line"></span>
+<span class="line"><span class="token punctuation">[</span>link<span class="token punctuation">]</span><span class="token punctuation">(</span>www.url.com<span class="token punctuation">)</span> ← assim cria links clicaveis</span>
+<span class="line"></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Caso a especificação acima não seja suficiente, você pode <a href="https://docs.pipz.com/central-de-ajuda/learning-center/guia-basico-de-markdown#open" target="_blank" rel="noopener noreferrer">clicar aqui</a> para ver um tutorial mais bem detalhado!</p><hr><p>Qualquer dúvida, entre em contato com: <code>carlos.eduardo@i9ss.com.br</code></p>`,7)])])}const r=s(l,[["render",t]]),d=JSON.parse('{"path":"/","title":"Portal de suporte Ginfo","lang":"en-US","frontmatter":{},"git":{"updatedTime":1773922490000,"contributors":[{"name":"Eduardo Basquiat","username":"","email":"flatbed@tutanota.com","commits":1},{"name":"Carlos Eduardo","username":"","email":"flatbed@tutanota.com","commits":1}],"changelog":[{"hash":"5f4a91b91f18afd8105f120eaf92a183a5c4762a","time":1773922490000,"email":"flatbed@tutanota.com","author":"Carlos Eduardo","message":"Alterações no index, também inclui um tutorial de md."},{"hash":"cbd54338f0a08d601b778d0612181c6212236476","time":1773754478000,"email":"flatbed@tutanota.com","author":"Eduardo Basquiat","message":"new changes"}]},"filePathRelative":"index.md"}');export{r as comp,d as data};
